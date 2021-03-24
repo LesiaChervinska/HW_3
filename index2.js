2a9cdb2192ba99b9b447b205fd6323b6a4055071
LesiaChervinska <zlesia2016@gmail.com>
2021-03-24 20:15:43 +0200

    add BEM, reset.css

--- index2.js
+++ index2.js
@@ -1,45 +1,55 @@
 var app = document.querySelector(".app");
 
-
 var buttonStartGame = document.createElement("button");
 buttonStartGame.innerHTML = "Давай сыиграем";
+buttonStartGame.setAttribute("class", "app__button");
 
 app.appendChild(buttonStartGame);
 
 var container = document.createElement("div");
+container.setAttribute("class", "app__container");
 var container2 = document.createElement("div");
-
+container2.setAttribute("class", "app__container2");
 var minRange = document.createElement("input");
 var maxRange = document.createElement("input");
 var attemps = document.createElement("input");
 minRange.setAttribute("type", "number");
+minRange.setAttribute("class", "app__input");
 minRange.setAttribute("value", "1");
 maxRange.setAttribute("type", "number");
+maxRange.setAttribute("class", "app__input");
 maxRange.setAttribute("value", "100");
 attemps.setAttribute("type", "number");
 attemps.setAttribute("value", "5");
+attemps.setAttribute("class", "app__input");
 
 var buttonContinue = document.createElement("button");
 buttonContinue.innerHTML = "Подтвердить выбор";
+buttonContinue.setAttribute("class", "app__button");
 
 
 var sectionGess = document.createElement("div");
+
 var inputGessNumber = document.createElement("input");
 inputGessNumber.setAttribute("type", "number");
+inputGessNumber.setAttribute("class", "app__input");
 
 
 buttonGessNumber = document.createElement("button");
 buttonGessNumber.innerHTML = "Ну начнем угадывать";
+buttonGessNumber.setAttribute("class", "app__button");
 
 buttonTryAgain = document.createElement('button');
 buttonTryAgain.innerHTML = "Попробовать еще раз";
+buttonTryAgain.setAttribute("class", "app__button");
 
 var resultDiv = document.createElement('div');
 var arrResult = document.createElement('div');
 
 
 var closeGame = document.createElement('button');
-closeGame.innerHTML = "Закончить игру"
+closeGame.innerHTML = "Закончить игру";
+closeGame.setAttribute("class", "app__button");
 
 
 var randomNumber = 0;
@@ -48,6 +58,7 @@ var counter = 0;
 
 buttonStartGame.onclick = function () {
     app.innerHTML ="Выбери диапазон от 0 до 200,количество попыток от 1 до 15 и попрбуй угдать число)";
+    var textP = document.createElement('p').innerHTML = "Выбери диапазон от 0 до 200,количество попыток от 1 до 15 и попрбуй угдать число)"
     container.appendChild(minRange);
     container.appendChild(maxRange);
     container.appendChild(attemps);
@@ -88,21 +99,7 @@ buttonContinue.onclick = function(){
     return randomNumber;
   }
 
-
-  function isValid(num1, num2, num3) {
-    if (num1 < 1 ||num1 > 200 ||
-      num2 > 200 ||num2 < 1 ||
-      num1 >= num2 ||
-      num3 < 1 ||num3 > 15 ) {
-      return false;
-    } else if (num1 % 1 != 0 || num2 % 1 != 0 || num3 % 1 != 0) {
-      return false;
-    }
-    return true;
-  }
-
   buttonGessNumber.addEventListener('click',function(){
-  //  console.log(randomNumber)
     var userNum = +(inputGessNumber.value);
     var namDiffernrt = Math.abs(userNum -randomNumber);
     attemps--;
@@ -129,15 +126,31 @@ buttonContinue.onclick = function(){
         }
     }
     else{
-        resultDiv.innerHTML = "Вы проиграли,страница перезагрузится через 2 секунды";
+        resultDiv.innerHTML = "Вы проиграли,страница перезагрузится через 5 секунд";
         buttonGessNumber.disabled = true;
+        buttonGessNumber.setAttribute('class', "app__button-disabled")
         setTimeout(()=>{
             window.location.reload()
-        },2000);
+        },5000);
     }
     counter++;
     app.append(resultDiv);
   })
+
+
+  
+  function isValid(num1, num2, num3) {
+    if (num1 < 1 ||num1 > 200 ||
+      num2 > 200 ||num2 < 1 ||
+      num1 >= num2 ||
+      num3 < 1 ||num3 > 15  ) {
+      return false;
+    } else if (num1 % 1 != 0 || num2 % 1 != 0 || num3 % 1 != 0) {
+      return false;
+    }
+    return true;
+  }
+
 
   closeGame.onclick = function(){
     window.location.reload();
